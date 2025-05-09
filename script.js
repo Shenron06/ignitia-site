a923cfe9d92b67a85012fdf5854fc6844bdba2e9
Cyril B. <cyrille59700@hotmail.fr>
2025-05-09 19:49:19 +0200

    Initial commit du site Ignitia

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fichier JavaScript de base
+console.log("Site chargé. Prêt pour vos créations IA.");
+
+// Tu pourras ajouter ici des interactions personnalisées plus tard,
+// comme un bouton 'copier lien', des filtres de galerie, ou un système de tri.
